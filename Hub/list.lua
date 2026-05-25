ef0d75522493b7280238b4a1e8190ab639814983
BROTHAISHACKIER <freemoviesondrive.1@gmail.com>
2026-05-25 08:25:03 +0200

    Create list.lua

---
+++ Hub/list.lua
@@ -0,0 +1,10 @@
+return {
+  scripts = {
+    ["FE Flex Your FPS"] = "https://gist.githubusercontent.com/BROTHAISHACKIER/1ac301a4a4119a6ff50b87544c17351d/raw",
+  },
+  profiles = {
+    ["ScriptBlox"] = "https://scriptblox.com/u/BROTHAISHACKIER",
+    ["Rscripts"] = "https://rscripts.net/@BIH",
+    ["HaxHell"] = "https://haxhell.com/user/brothaishackier",
+  }
+}
